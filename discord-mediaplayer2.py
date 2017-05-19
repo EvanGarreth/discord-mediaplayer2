35261e51f2e066f15af83e8b2580fd4362ec39d4
EvanGarreth <evangarreth@gmail.com>
2017-05-19 15:38:26 -0500

    Add current project code

---
+++ discord-mediaplayer2.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+import asyncio, dbus, discord
+
+SECONDS_BETWEEN_CHECKS = 10
+# In the discord client, hold ctrl+shift+i and look for the token value in Local Storage under the Application tab
+CLIENT_TOKEN = "REPLACE_THIS"
+# The MediaPlayer2 service name. Can be changed to any MediaPlayer2 compatible platform (ex: Rythmbox, VLC, Spotfiy)
+SERVICE = "org.mpris.MediaPlayer2.spotify"
+
+client = discord.Client()
+currentGame = None
+playerInterface = None
+
+@client.event
+async def on_ready():
+    displayUserInfo()
+    await checkCurrentlyPlayingSong()
+
+async def checkCurrentlyPlayingSong():
+    while True:
+        if playerIsRunning():
+            hookPlayerInterface()
+            game = createGame()
+            if game != currentGame: # Perform this check so that we don't spam the API with a redundant status
+                await updateUserGame(game)
+        await asyncio.sleep(SECONDS_BETWEEN_CHECKS)
+
+def playerIsRunning():
+    return dbus.SessionBus().name_has_owner(SERVICE)
+
+def hookPlayerInterface():
+    global playerInterface
+    playerBus = dbus.SessionBus().get_object(SERVICE, "/org/mpris/MediaPlayer2")
+    playerInterface = dbus.Interface(playerBus, "org.freedesktop.DBus.Properties")
+
+async def updateUserGame(newGame):
+    global currentGame
+    print("Changing {} to {}".format(str(currentGame), str(newGame)))
+    currentGame = newGame
+    await client.change_presence(game=newGame)
+
+def createGame():
+    metaData = playerInterface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    status = "♪ {} - {}".format(metaData["xesam:title"], metaData["xesam:artist"][0])
+    return discord.Game(name=status)
+
+def displayUserInfo():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+
+if __name__ == "__main__":
+    client.run(CLIENT_TOKEN, bot=False)
